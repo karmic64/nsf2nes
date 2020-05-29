3b8a41f3c21da7093c25bcee1f72951cc047a8ec
karmic <karmic.c64@gmail.com>
2020-05-29 13:13:21 -0600

    first

---
+++ README.md
@@ -0,0 +1,26 @@
+# nsf2nes
+No-frills NSF rip to NES ROM converter.
+
+
+## Usage
+First, know the limitations of the NSFs you can use.
+- The NSF may not use bankswitching.
+- The NSF may not use expansion sound.
+- There must be at least one free page of memory for the 6502 driver code.
+If any of these conditions are not satisfied, an error message will be printed and no ROM will be written.
+
+Usage is nothing more than `nsf2nes nsffile`. If everything went well, an NES ROM will be created with the same filename as the NSF with `.nes` appended to it.
+
+When you load the ROM, use the D-Pad to step through songs and any other button to restart the current one.
+
+
+## Build info
+If you want to modify the driver, it's in [64tass](tass64.sourceforge.net) syntax. `64tass -f -o drv.bin drv.asm` to assemble it. Keep it under 250 bytes.
+
+The method I use of including the driver in the executable might be incompatible with some compilers. If you have any problems, please contact me.
+
+
+## To-do
+- Support optimized NSFs which don't actually use bankswitching for anything but reducing the filesize
+- Detection of bloated rips that simply fill all of $8000-$FFFF, placing driver in an unused page
+- Mapper 31-specific driver for full NSF bankswitching (maybe)
---
+++ drv.asm
@@ -0,0 +1,123 @@
+tuneid  = $01ff
+rawjoy  = $01fe
+region  = $01fd
+nmiflag = $01fc
+stack   = $01fb
+
+
+init    = $4242
+play    = $4343
+maxtune = $44
+deftune = $45
+        
+        
+        .logical $4100
+        inc nmiflag
+        rti
+        
+        
+        ldx #<stack
+        txs
+-       bit $2002
+        bpl *-3
+        inx
+        bne -
+        lda #$80
+        sta $2000
+        ldy #0
+        sty rawjoy
+        lda nmiflag
+-       cmp nmiflag
+        bne +
+        inx
+        bne -
+        iny
+        bne -
++       cpy #$b
+        lda #0
+        rol
+        sta region
+        lda #deftune
+        sta tuneid
+        
+clear   lda #7
+        sta 1
+        lda #0
+        sta $4015
+        sta $4017
+        tay
+        sta 0
+        ldx #6
+-       sta (0),y
+        iny
+        bne -
+        dec 1
+        dex
+        bne -
+-       sta 0,x
+        sta @wstack-$ff,x
+        inx
+        bne -
+        lda #$0f
+        sta $4015
+        lda tuneid
+        ldx region
+        jsr init
+        
+main    lda nmiflag
+        cmp nmiflag
+        beq *-3
+        jsr play
+        
+        ldy rawjoy
+        jsr readjoy
+-       lda rawjoy
+        pha
+        jsr readjoy
+        pla
+        cmp rawjoy
+        bne -
+        tya
+        eor #$ff
+        and rawjoy
+        tay
+        ldx tuneid
+        bit joyinc
+        beq +
+        inx
+        cpx #maxtune
+        bcc +
+        ldx #0
++       bit joydec
+        beq ++
+        txa
+        bne +
+        ldx #maxtune
++       dex
++       stx tuneid
+        tya
+        bne clear
+        beq main
+        
+        
+readjoy ldx #1
+        stx $4016
+        stx rawjoy
+        dex
+        stx $4016
+-       lda $4016
+        and #3
+        cmp #1
+        rol rawjoy
+        bcc -
+        rts
+        
+joyinc  .byte $29
+joydec  .byte $06
+
+
+        
+        
+        .here
+        
+        
